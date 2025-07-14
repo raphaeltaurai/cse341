4b0a810dac9f0cb579be17f8b2d548193e37ad5c
raphaeltaurai <shawntaurai@gmail.com>
2025-07-14 16:38:00 +0200

    .

--- backend/routes/professional.js
+++ backend/routes/professional.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const professionalController = require("../controllers.professional");
+const professionalController = require('../controllers.professional');
 
 const router = express.Router();
 
 //GET  /feed /post
-router.get("/", professionalController.getProfessionalsData);
+router.get("/", professionalController.getData);
 //localhost:8080/professionals
 module.exports = router;
